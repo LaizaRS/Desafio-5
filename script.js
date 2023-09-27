3558a469d1dd82b0169600dbccdbb5b1abd58f41
Laiza <l.aiza.saraiva@gmail.com>
2023-09-27 10:09:28 -0300

    os calculos funcionam

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-
 function atualizarP() {
   var div = document.getElementById("vazio");
   var span = document.getElementById("total");
-
-  // primeiro passo, pensar na logica antes de programar
-  // segundo passo, pensar na melhor logica antes de programar
-  // identificar o que deve fazer quando clicar em cada botão.
-  // Quando clicar em um numero ou operador irá chamar uma função 
-  // que irá colocar dentro de uma variavel do tipo string o que foi digitado
-  // Quando clicar no igual irá dar o total do que foi digitado
-  
 
   var numeroString = '';
   var numeroNumero = 0;
@@ -22,132 +13,147 @@ function atualizarP() {
 
   var tabela = document.getElementById("minhaTabela");
 
+  function adicionarNumero(numero) {
+    if (numeroString === "0") {
+      numeroString = numero;
+    } else {
+      numeroString += numero;
+    }
+  }
+
+  function soUm () {
+    var cliqueHabilitado = true;
+
+    if (cliqueHabilitado) {
+      
+    }
+  }
+
   tabela.addEventListener("click", function(event) {
     if (event.target.tagName === "TD") {
       var acao = event.target.getAttribute("data-action");
-  
+
       switch (acao) {
         case "acaoCE":
-          if (numeroString === "0") {
-          } else {
-            numeroString = 0;
-            total = 0; 
+          numeroString = "0";
+          total = 0;
+          adicao = 0;
+          subtracao = 0;
+          multiplicacao = 0;
+          divisao = 0;
+          porcento = 0;
+          break;
+        case "acaoC":
+          numeroString = "0";
+          total = 0;
+          adicao = 0;
+          subtracao = 0;
+          multiplicacao = 0;
+          divisao = 0;
+          porcento = 0;
+          break;
+
+        case "acao0":
+          adicionarNumero("0");
+          break;
+        case "acao1":
+          adicionarNumero("1");
+          break;
+        case "acao2":
+          adicionarNumero("2");
+          break;
+        case "acao3":
+          adicionarNumero("3");
+          break;
+        case "acao4":
+          adicionarNumero("4");
+          break;
+        case "acao5":
+          adicionarNumero("5");
+          break;
+        case "acao6":
+          adicionarNumero("6");
+          break;
+        case "acao7":
+          adicionarNumero("7");
+          break;
+        case "acao8":
+          adicionarNumero("8");
+          break;
+        case "acao9":
+          adicionarNumero("9");
+          break;
+
+        case "acaoAdicao":
+          numeroNumero = parseInt(numeroString);
+          numeroString = '';
+          adicao += numeroNumero;
+          subtracao = 0;
+          multiplicacao = 0;
+          divisao = 0;
+          porcento = 0;
+          break;
+        case "acaoSubtracao":
+          numeroNumero = parseInt(numeroString);
+          numeroString = '';
+          subtracao += numeroNumero;
+          console.log(subtracao)
+          adicao = 0;
+          multiplicacao = 0;
+          divisao = 0;
+          porcento = 0;
+          break;
+        case "acaoMultiplicacao":
+          numeroNumero = parseInt(numeroString);
+          numeroString = '';
+          multiplicacao = numeroNumero;
+          adicao = 0;
+          subtracao = 0;
+          divisao = 0;
+          porcento = 0;
+          break;
+        case "acaoDivisao":
+          numeroNumero = parseInt(numeroString);
+          numeroString = '';
+          divisao += numeroNumero;
+          adicao = 0;
+          subtracao = 0;
+          multiplicacao = 0;
+          porcento = 0;
+          break;
+        case "acaoPorcento":
+          numeroNumero = parseInt(numeroString) / 100;
+          numeroString = '';
+          porcento += numeroNumero;
+          adicao = 0;
+          subtracao = 0;
+          multiplicacao = 0;
+          divisao = 0;
+          break;
+
+        case "acaoIgual":
+          if (numeroNumero !== 0) {
+            if (adicao) {
+              total = adicao + parseInt(numeroString);
+            } else if (subtracao) {
+              total =  subtracao - parseInt(numeroString);
+            } else if (multiplicacao) {
+              total = multiplicacao * parseInt(numeroString);
+            } else if (divisao) {
+              total = divisao / parseInt(numeroString) ;
+            } else if (porcento) {
+              total = porcento * parseInt(numeroString);
+            }
           }
+          console.log ("numeroNumero", numeroNumero, 'numeroString',numeroString, "total",total )
           div.innerHTML = numeroString;
           span.innerHTML = total;
           break;
-          case "acaoC":
-            if (numeroString === "0") {
-            } else {
-              numeroString = 0;
-              total = 0; 
-            }
-            div.innerHTML = numeroString;
-            span.innerHTML = total;
-            break;
-
-        case "acao0":
-          if (numeroString === "0") {
-            // Evite definir "0" repetidamente
-          } else {
-            numeroString = "0";
-          }
-          break;
-          case "acao1":
-          numeroString += "1";
-          break;
-          case "acao2":
-          numeroString += "2";
-          break;
-          case "acao3":
-          numeroString += "3";
-          break;
-          case "acao4":
-          numeroString += "4";
-          break;
-          case "acao5":
-          numeroString += "5";
-          break;
-          case "acao6":
-          numeroString += "6";
-          break;
-          case "acao7":
-          numeroString += "7";
-          break;
-          case "acao8":
-          numeroString += "8";
-          break;
-          case "acao9":
-          numeroString += "9";
-          break;
-
-
-
-          case "acaoAdicao":
-            if (numeroString === "0") {
-            } else {
-              numeroNumero = parseInt(numeroString);
-              numeroString = ''; 
-              adicao += numeroNumero;
-              console.log("Adição realizada:", adicao);
-            }
-            break;
-         case "acaoSubtracao":
-          if (numeroString === "0") {
-          } else {
-            numeroNumero = parseInt(numeroString);
-            numeroString = '';
-            subtracao += numeroNumero;
-          }
-         break;
-         case "acaoMultiplicacao":
-          if (numeroString === "0") {
-          } else {
-            numeroNumero = parseInt(numeroString);
-            numeroString = '';
-            multiplicacao = numeroNumero
-          }
-         break;
-         case "acaoDivisao":
-          if (numeroString === "0") {
-          } else {
-            numeroNumero = parseInt(numeroString);
-            numeroString = '';
-            divisao += numeroNumero;
-          }
-         break;
-         case "acaoPorcento":
-          if (numeroString === "0") {
-          } else {
-            numeroNumero = parseInt(numeroString)/100;
-            numeroString = '';
-            porcento += numeroNumero;
-          }
-          break;
-
-          case "acaoIgual":
-            if (numeroNumero !== 0) {
-              if (adicao) {
-                total = numeroNumero + adicao;
-              } else if (subtracao) {
-                total = numeroNumero - subtracao;
-              } else if (multiplicacao) {
-                total = numeroNumero * multiplicacao;
-              } else if (divisao) {
-                total = numeroNumero / divisao;
-              } else if (porcento) {
-                total = numeroNumero * porcento;
-              }
-            }
-            console.log(numeroNumero, divisao)
-            div.innerHTML = numeroString;
-            span.innerHTML = total;
-            break;
-        
-          default:
-            div.innerHTML = numeroString;
-            span.innerHTML = total;
-        }
+          
+        default:
+          div.innerHTML = numeroString;
+          span.innerHTML = total;
+      }
 
       div.innerHTML = numeroString;
       span.innerHTML = total;
@@ -155,5 +161,4 @@ function atualizarP() {
   });
 }
 
-
-atualizarP()
+atualizarP();
